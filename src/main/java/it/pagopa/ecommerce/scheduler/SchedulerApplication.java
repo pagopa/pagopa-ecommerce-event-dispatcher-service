51a0d8dc70c54f0d40fb6ff704044588ab5da5bc
MarcoAGreco <marcoandreagreco@gmail.com>
2022-07-07 16:32:15 +0200

    [CHK-510] feat: add sample scheduled task

--- src/main/java/it/pagopa/ecommerce/scheduler/SchedulerApplication.java
+++ src/main/java/it/pagopa/ecommerce/scheduler/SchedulerApplication.java
@@ -2,8 +2,10 @@ package it.pagopa.ecommerce.scheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SchedulerApplication {
 
 	public static void main(String[] args) {
