e32a4e8cf0685f3eaab365aa48a993e92d9ee4d1
MarcoAGreco <marcoandreagreco@gmail.com>
2022-07-11 11:24:52 +0200

    [CHK-510] feat: initial project setup

---
+++ src/main/kotlin/it/pagopa/ecommerce/scheduler/task/DemoTask.kt
@@ -0,0 +1,19 @@
+package it.pagopa.ecommerce.scheduler.task
+
+import org.slf4j.LoggerFactory
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+@Component
+class DemoTask {
+    private val logger = LoggerFactory.getLogger(javaClass)
+    private val dateFormat = SimpleDateFormat("HH:mm:ss")
+
+    @Scheduled(fixedRate = 5000)
+    fun reportCurrentTime() {
+        logger.info("Demo Scheduled task: execution time: {}", dateFormat.format(Date()))
+    }
+}
