aa57bc5a5384f2d6d100f21966d22f2beea06483
Marco Greco <marcoandreagreco@gmail.com>
2022-07-12 14:36:27 +0200

    [CHK-510] Project setup (#2)
    * [CHK-510] feat: initial project setup
    
    * [CHK-510] feat: initial project setup
    
    * [CHK-510] fix: update docker-compose.yaml env file

---
+++ src/main/kotlin/it/pagopa/ecommerce/scheduler/config/Config.kt
@@ -0,0 +1,10 @@
+package it.pagopa.ecommerce.scheduler.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+
+@Configuration
+@EnableScheduling
+class Config {
+}
