ac2ff27ad899ff0cef67480a38b8e674c2328866
Giovanni Berti <giovanni.berti@pagopa.it>
2022-07-14 10:03:14 +0200

    feat(node): add node closepayment service

--- src/test/kotlin/it/pagopa/ecommerce/scheduler/SchedulerApplicationTests.kt
+++ src/test/kotlin/it/pagopa/ecommerce/scheduler/SchedulerApplicationTests.kt
@@ -2,8 +2,10 @@ package it.pagopa.ecommerce.scheduler
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestPropertySource
 
 @SpringBootTest
+@TestPropertySource(locations = ["classpath:application-tests.properties"])
 class SchedulerApplicationTests {
 
 	@Test
