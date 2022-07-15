15e622bc05a7e3887e6287a2765a77534ae13b66
Simone Infante <simone.infante@pagopa.it>
2022-07-15 08:54:32 +0200

    fix: remove old import

--- src/main/kotlin/it/pagopa/ecommerce/scheduler/queues/TransactionAuthRequestedEventsConsumer.kt
+++ src/main/kotlin/it/pagopa/ecommerce/scheduler/queues/TransactionAuthRequestedEventsConsumer.kt
@@ -2,18 +2,12 @@ package it.pagopa.ecommerce.scheduler.queues
 
 import com.azure.spring.messaging.AzureHeaders
 import com.azure.spring.messaging.checkpoint.Checkpointer
-import com.azure.spring.messaging.storage.queue.core.StorageQueueTemplate
-import com.azure.spring.integration.storage.queue.inbound.StorageQueueMessageSource
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.integration.annotation.ServiceActivator
-import org.springframework.integration.annotation.InboundChannelAdapter
-import org.springframework.integration.annotation.Poller
-import org.springframework.integration.channel.DirectChannel
 import org.springframework.messaging.handler.annotation.Header
 import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.stereotype.Service
-import org.springframework.context.annotation.Bean
 
 @Service
 class TransactionAuthRequestedEventsConsumer() {
