23a8ce89ca8ec85c8de3c2286678a7ec46d11bb0
Simone Infante <simone.infante@pagopa.it>
2022-07-15 08:44:24 +0200

    feat: update base test

--- src/test/kotlin/it/pagopa/ecommerce/scheduler/SchedulerApplicationTests.kt
+++ src/test/kotlin/it/pagopa/ecommerce/scheduler/SchedulerApplicationTests.kt
@@ -3,11 +3,21 @@ package it.pagopa.ecommerce.scheduler
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.TestPropertySource
+import org.springframework.beans.factory.annotation.Autowired
+import com.azure.spring.integration.storage.queue.inbound.StorageQueueMessageSource
+import com.azure.spring.messaging.storage.queue.core.StorageQueueTemplate
 
 @SpringBootTest
 @TestPropertySource(locations = ["classpath:application.test.properties"])
 class SchedulerApplicationTests {
 
+	@Autowired 
+	lateinit var storageQueueMessageSource: StorageQueueMessageSource 
+
+	@Autowired 
+	lateinit var storageQueueTemplate: StorageQueueTemplate
+
+	
 	@Test
 	fun contextLoads() {
 	}
