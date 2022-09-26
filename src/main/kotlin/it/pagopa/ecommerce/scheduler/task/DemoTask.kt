ebf75e0e724a784287565ec8ddf8b2fc064ef663
Simone Infante <simone.infante@pagopa.it>
2022-09-26 09:43:01 +0200

    test deploy uat

--- src/main/kotlin/it/pagopa/ecommerce/scheduler/task/DemoTask.kt
+++ src/main/kotlin/it/pagopa/ecommerce/scheduler/task/DemoTask.kt
@@ -12,8 +12,8 @@ class DemoTask {
     private val logger = LoggerFactory.getLogger(javaClass)
     private val dateFormat = SimpleDateFormat("HH:mm:ss")
 
-    @Scheduled(fixedRate = 5000)
-    fun reportCurrentTime() {
+    //@Scheduled(fixedRate = 5000)
+    private fun reportCurrentTime() {
         logger.info("Demo Scheduled task: execution time: {}", dateFormat.format(Date()))
     }
 }
