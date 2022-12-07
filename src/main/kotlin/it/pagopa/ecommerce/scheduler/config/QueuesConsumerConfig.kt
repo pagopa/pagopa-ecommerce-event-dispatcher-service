90255c70c1e1a6f4d21f0f0fd178251c2f0e894a
Giovanni Berti <giovanni.berti@pagopa.it>
2022-12-07 10:12:46 +0100

    feat(retry-close): add config for closure retry queue

--- src/main/kotlin/it/pagopa/ecommerce/scheduler/config/QueuesConsumerConfig.kt
+++ src/main/kotlin/it/pagopa/ecommerce/scheduler/config/QueuesConsumerConfig.kt
@@ -12,10 +12,6 @@ import org.springframework.beans.factory.annotation.Value
 
 @Configuration
 class QueuesConsumerConfig {
-
-    @Value("\${azurestorage.queues.transactionactivatedevents.name}")
-    private val queueNameActivatedEvents: String? = null
-
     @Bean
     fun input(): DirectChannel {
         return DirectChannel()
@@ -23,7 +19,19 @@ class QueuesConsumerConfig {
 
     @Bean
     @InboundChannelAdapter(channel = "transactionactivatedchannel", poller = [Poller(fixedDelay = "1000")])
-    fun storageQueueActivatedMessageSource(storageQueueTemplate: StorageQueueTemplate): StorageQueueMessageSource {
+    fun storageQueueActivatedMessageSource(
+        storageQueueTemplate: StorageQueueTemplate,
+        @Value("\${azurestorage.queues.transactionactivatedevents.name}") queueNameActivatedEvents: String
+    ): StorageQueueMessageSource {
         return StorageQueueMessageSource(queueNameActivatedEvents, storageQueueTemplate)
+    }
+
+    @Bean
+    @InboundChannelAdapter(channel = "transactionclosureschannel", poller = [Poller(fixedDelay = "1000")])
+    fun storageQueueClosuresMessageSource(
+        storageQueueTemplate: StorageQueueTemplate,
+        @Value("\${azurestorage.queues.transactionclosuresentevents.name}") queueNameClosureEvents: String
+    ): StorageQueueMessageSource {
+        return StorageQueueMessageSource(queueNameClosureEvents, storageQueueTemplate)
     }
 }
