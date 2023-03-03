0780e1eafa550621661c4b30d794557c0f2dec55
Simone Infante <simone.infante@pagopa.it>
2023-03-03 20:15:13 +0100

    fix: test format

--- src/test/kotlin/it/pagopa/ecommerce/scheduler/queues/TransactionsRefundEventsConsumerTests.kt
+++ src/test/kotlin/it/pagopa/ecommerce/scheduler/queues/TransactionsRefundEventsConsumerTests.kt
@@ -10,8 +10,7 @@ import it.pagopa.ecommerce.scheduler.client.PaymentGatewayClient
 import it.pagopa.ecommerce.scheduler.repositories.TransactionsEventStoreRepository
 import it.pagopa.ecommerce.scheduler.repositories.TransactionsViewRepository
 import it.pagopa.generated.ecommerce.gateway.v1.dto.PostePayRefundResponseDto
-import it.pagopa.generated.ecommerce.nodo.v2.dto.ClosePaymentRequestV2Dto
-import it.pagopa.generated.ecommerce.nodo.v2.dto.ClosePaymentResponseDto
+import java.util.*
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Test
@@ -21,8 +20,6 @@ import org.mockito.junit.jupiter.MockitoExtension
 import org.mockito.kotlin.*
 import reactor.core.publisher.Mono
 import reactor.kotlin.core.publisher.toFlux
-import java.time.ZonedDateTime
-import java.util.*
 
 @ExtendWith(MockitoExtension::class)
 @OptIn(ExperimentalCoroutinesApi::class)
@@ -48,58 +45,58 @@ class TransactionsRefundEventsConsumerTests {
     )
 
   @Test
-  fun `consumer processes refund request event correctly with pgs refund`() =
-    runTest {
-      val activationEvent = TransactionTestUtils.transactionActivateEvent() as TransactionEvent<Any>
-      val authorizationRequestEvent =
-        transactionAuthorizationRequestedEvent() as TransactionEvent<Any>
-      val authorizationCompleteEvent =
-        TransactionTestUtils.transactionAuthorizationCompletedEvent() as TransactionEvent<Any>
-      val refundRequestedEvent = TransactionRefundRequestedEvent(
+  fun `consumer processes refund request event correctly with pgs refund`() = runTest {
+    val activationEvent = TransactionTestUtils.transactionActivateEvent() as TransactionEvent<Any>
+    val authorizationRequestEvent =
+      transactionAuthorizationRequestedEvent() as TransactionEvent<Any>
+    val authorizationCompleteEvent =
+      TransactionTestUtils.transactionAuthorizationCompletedEvent() as TransactionEvent<Any>
+    val refundRequestedEvent =
+      TransactionRefundRequestedEvent(
         TransactionTestUtils.TRANSACTION_ID,
-        TransactionRefundedData(TransactionStatusDto.REFUND_REQUESTED)
-      ) as TransactionEvent<Any>
+        TransactionRefundedData(TransactionStatusDto.REFUND_REQUESTED))
+        as TransactionEvent<Any>
 
-      val gatewayClientResponse = PostePayRefundResponseDto().apply { refundOutcome = "OK" }
+    val gatewayClientResponse = PostePayRefundResponseDto().apply { refundOutcome = "OK" }
 
-      val events =
-        listOf(
-          activationEvent, authorizationRequestEvent, authorizationCompleteEvent, refundRequestedEvent)
+    val events =
+      listOf(
+        activationEvent,
+        authorizationRequestEvent,
+        authorizationCompleteEvent,
+        refundRequestedEvent)
 
-      // not working
-      val expectedRefundedEvent =
-        TransactionRefundedEvent(
-          TransactionTestUtils.TRANSACTION_ID, TransactionRefundedData(TransactionStatusDto.AUTHORIZATION_COMPLETED)
-        )
+    // not working
+    val expectedRefundedEvent =
+      TransactionRefundedEvent(
+        TransactionTestUtils.TRANSACTION_ID,
+        TransactionRefundedData(TransactionStatusDto.AUTHORIZATION_COMPLETED))
 
-      /* preconditions */
-      given(checkpointer.success()).willReturn(Mono.empty())
-      given(transactionsEventStoreRepository.findByTransactionId(any())).willReturn(events.toFlux())
-      given(transactionsViewRepository.save(any())).willAnswer { Mono.just(it.arguments[0]) }
-      given(transactionsRefundedEventStoreRepository.save(any()))
-        .willReturn(Mono.just(expectedRefundedEvent))
-      given(paymentGatewayClient.requestRefund(any())).willReturn(Mono.just(gatewayClientResponse))
+    /* preconditions */
+    given(checkpointer.success()).willReturn(Mono.empty())
+    given(transactionsEventStoreRepository.findByTransactionId(any())).willReturn(events.toFlux())
+    given(transactionsViewRepository.save(any())).willAnswer { Mono.just(it.arguments[0]) }
+    given(transactionsRefundedEventStoreRepository.save(any()))
+      .willReturn(Mono.just(expectedRefundedEvent))
+    given(paymentGatewayClient.requestRefund(any())).willReturn(Mono.just(gatewayClientResponse))
 
-      /* test */
+    /* test */
 
-      val refundedEventId = UUID.fromString(expectedRefundedEvent.id)
+    val refundedEventId = UUID.fromString(expectedRefundedEvent.id)
 
-      Mockito.mockStatic(UUID::class.java).use { uuid ->
-        uuid.`when`<Any>(UUID::randomUUID).thenReturn(refundedEventId)
-        uuid.`when`<Any> { UUID.fromString(any()) }.thenCallRealMethod()
+    Mockito.mockStatic(UUID::class.java).use { uuid ->
+      uuid.`when`<Any>(UUID::randomUUID).thenReturn(refundedEventId)
+      uuid.`when`<Any> { UUID.fromString(any()) }.thenCallRealMethod()
 
-        transactionRefundedEventsConsumer
-          .messageReceiver(BinaryData.fromObject(refundRequestedEvent).toBytes(), checkpointer)
-          .block()
+      transactionRefundedEventsConsumer
+        .messageReceiver(BinaryData.fromObject(refundRequestedEvent).toBytes(), checkpointer)
+        .block()
 
-        /* Asserts */
-        verify(checkpointer, Mockito.times(1)).success()
-        verify(paymentGatewayClient, Mockito.times(1))
-          .requestRefund(UUID.fromString(TransactionTestUtils.TRANSACTION_ID))
-        verify(transactionsRefundedEventStoreRepository, Mockito.times(1))
-          .save(
-            any())
-      }
+      /* Asserts */
+      verify(checkpointer, Mockito.times(1)).success()
+      verify(paymentGatewayClient, Mockito.times(1))
+        .requestRefund(UUID.fromString(TransactionTestUtils.TRANSACTION_ID))
+      verify(transactionsRefundedEventStoreRepository, Mockito.times(1)).save(any())
     }
-
+  }
 }
