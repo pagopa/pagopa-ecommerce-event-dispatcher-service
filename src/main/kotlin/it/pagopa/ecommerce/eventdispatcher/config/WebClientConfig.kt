b01abefe162c72b6895798a3758e30ba3e416a0e
MarcoAGreco <marcoandreagreco@gmail.com>
2023-05-11 14:45:15 +0200

    [CHK-1454] fix: fmt

--- src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/config/WebClientConfig.kt
+++ src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/config/WebClientConfig.kt
@@ -2,20 +2,20 @@ package it.pagopa.ecommerce.eventdispatcher.config
 
 import io.netty.channel.ChannelOption
 import io.netty.handler.timeout.ReadTimeoutHandler
+import it.pagopa.generated.ecommerce.gateway.v1.ApiClient as GatewayApiClient
 import it.pagopa.generated.ecommerce.gateway.v1.api.VposApi
 import it.pagopa.generated.ecommerce.gateway.v1.api.XPayApi
+import it.pagopa.generated.ecommerce.nodo.v2.ApiClient as NodoApiClient
 import it.pagopa.generated.ecommerce.nodo.v2.api.NodoApi
 import it.pagopa.generated.notifications.v1.ApiClient
 import it.pagopa.generated.notifications.v1.api.DefaultApi
+import java.util.concurrent.TimeUnit
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import reactor.netty.Connection
 import reactor.netty.http.client.HttpClient
-import java.util.concurrent.TimeUnit
-import it.pagopa.generated.ecommerce.gateway.v1.ApiClient as GatewayApiClient
-import it.pagopa.generated.ecommerce.nodo.v2.ApiClient as NodoApiClient
 
 @Configuration
 class WebClientConfig {
