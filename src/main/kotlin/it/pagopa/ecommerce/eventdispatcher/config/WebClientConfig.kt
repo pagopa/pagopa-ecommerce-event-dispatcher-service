b04347dd9f6bb8acb1f20663bdf05fa8a77ca30f
Marco Greco <marcoandreagreco@gmail.com>
2023-05-17 17:04:18 +0200

    [CHK-1510] fix: drop null-value in body serialization (#78)

--- src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/config/WebClientConfig.kt
+++ src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/config/WebClientConfig.kt
@@ -1,5 +1,9 @@
 package it.pagopa.ecommerce.eventdispatcher.config
 
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
 import io.netty.channel.ChannelOption
 import io.netty.handler.timeout.ReadTimeoutHandler
 import it.pagopa.generated.ecommerce.gateway.v1.ApiClient as GatewayApiClient
@@ -13,12 +17,26 @@ import java.util.concurrent.TimeUnit
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.core.codec.StringDecoder
+import org.springframework.http.MediaType
 import org.springframework.http.client.reactive.ReactorClientHttpConnector
+import org.springframework.http.codec.ClientCodecConfigurer
+import org.springframework.http.codec.json.Jackson2JsonDecoder
+import org.springframework.http.codec.json.Jackson2JsonEncoder
+import org.springframework.web.reactive.function.client.ExchangeStrategies
 import reactor.netty.Connection
 import reactor.netty.http.client.HttpClient
 
 @Configuration
 class WebClientConfig {
+
+  fun getNodeObjectMapper(): ObjectMapper {
+    val mapper = ObjectMapper()
+    mapper.registerModule(JavaTimeModule())
+    mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+    mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL)
+    return mapper
+  }
   @Bean
   fun nodoApi(
     @Value("\${nodo.uri}") nodoUri: String,
@@ -32,9 +50,25 @@ class WebClientConfig {
           connection.addHandlerLast(ReadTimeoutHandler(nodoReadTimeout, TimeUnit.MILLISECONDS))
         }
 
+    val exchangeStrategies =
+      ExchangeStrategies.builder()
+        .codecs { clientCodecConfigurer: ClientCodecConfigurer ->
+          val mapper = getNodeObjectMapper()
+          clientCodecConfigurer.registerDefaults(false)
+          clientCodecConfigurer.customCodecs().register(StringDecoder.allMimeTypes())
+          clientCodecConfigurer
+            .customCodecs()
+            .register(Jackson2JsonDecoder(mapper, MediaType.APPLICATION_JSON))
+          clientCodecConfigurer
+            .customCodecs()
+            .register(Jackson2JsonEncoder(mapper, MediaType.APPLICATION_JSON))
+        }
+        .build()
+
     val webClient =
       NodoApiClient.buildWebClientBuilder()
         .clientConnector(ReactorClientHttpConnector(httpClient))
+        .exchangeStrategies(exchangeStrategies)
         .baseUrl(nodoUri)
         .build()
 
---
+++ src/test/kotlin/it/pagopa/ecommerce/eventdispatcher/config/WebClientConfigTest.kt
@@ -0,0 +1,92 @@
+package it.pagopa.ecommerce.eventdispatcher.config
+
+import it.pagopa.generated.ecommerce.nodo.v2.dto.AdditionalPaymentInformationsDto
+import it.pagopa.generated.ecommerce.nodo.v2.dto.ClosePaymentRequestV2Dto
+import java.math.BigDecimal
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
+import java.time.temporal.ChronoUnit
+import java.util.List
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.InjectMocks
+import org.mockito.junit.jupiter.MockitoExtension
+
+@ExtendWith(MockitoExtension::class)
+@OptIn(ExperimentalCoroutinesApi::class)
+internal class WebClientConfigTest {
+  @InjectMocks private lateinit var webClientsConfig: WebClientConfig
+
+  @Test
+  fun `should correctly serialize closePayment request`() = runTest {
+
+    // Precondition
+    val additionalPaymentInformationsDto =
+      AdditionalPaymentInformationsDto()
+        .outcomePaymentGateway(AdditionalPaymentInformationsDto.OutcomePaymentGatewayEnum.OK)
+        .totalAmount(BigDecimal(101).toString())
+        .rrn("rrn")
+        .fee(BigDecimal(1).toString())
+        .timestampOperation(
+          OffsetDateTime.now()
+            .truncatedTo(ChronoUnit.SECONDS)
+            .format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+
+    val closePaymentRequest =
+      ClosePaymentRequestV2Dto()
+        .paymentTokens(List.of("paymentToken"))
+        .outcome(ClosePaymentRequestV2Dto.OutcomeEnum.OK)
+        .idPSP("identificativoPsp")
+        .idBrokerPSP("identificativoIntermediario")
+        .idChannel("identificativoCanale")
+        .transactionId("transactionId")
+        .fee(BigDecimal(1))
+        .timestampOperation(OffsetDateTime.now())
+        .totalAmount(BigDecimal(101))
+        .additionalPaymentInformations(additionalPaymentInformationsDto)
+
+    val mapper = webClientsConfig.getNodeObjectMapper()
+    val jsonRequest = mapper.writeValueAsString(closePaymentRequest)
+
+    // Asserts
+    Assertions.assertThat(jsonRequest).contains("\"rrn\":\"rrn\"")
+  }
+
+  @Test
+  fun `should correctly serialize closePayment request with null value`() = runTest {
+
+    // Precondition
+    val additionalPaymentInformationsDto =
+      AdditionalPaymentInformationsDto()
+        .outcomePaymentGateway(AdditionalPaymentInformationsDto.OutcomePaymentGatewayEnum.OK)
+        .totalAmount(BigDecimal(101).toString())
+        .rrn(null)
+        .fee(BigDecimal(1).toString())
+        .timestampOperation(
+          OffsetDateTime.now()
+            .truncatedTo(ChronoUnit.SECONDS)
+            .format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
+
+    val closePaymentRequest =
+      ClosePaymentRequestV2Dto()
+        .paymentTokens(List.of("paymentToken"))
+        .outcome(ClosePaymentRequestV2Dto.OutcomeEnum.OK)
+        .idPSP("identificativoPsp")
+        .idBrokerPSP("identificativoIntermediario")
+        .idChannel("identificativoCanale")
+        .transactionId("transactionId")
+        .fee(BigDecimal(1))
+        .timestampOperation(OffsetDateTime.now())
+        .totalAmount(BigDecimal(101))
+        .additionalPaymentInformations(additionalPaymentInformationsDto)
+
+    val mapper = webClientsConfig.getNodeObjectMapper()
+    val jsonRequest = mapper.writeValueAsString(closePaymentRequest)
+
+    // Asserts
+    Assertions.assertThat(jsonRequest).doesNotContain("rrn")
+  }
+}
