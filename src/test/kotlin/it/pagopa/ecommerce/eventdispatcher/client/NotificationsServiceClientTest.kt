1257a78f9825a604f5c6114247049a5bd58541cd
Pietro Tota <pietro.tota@pagopa.it>
2023-05-22 17:33:54 +0200

    fix: add tests for coverage

--- src/test/kotlin/it/pagopa/ecommerce/eventdispatcher/client/NotificationsServiceClientTest.kt
+++ src/test/kotlin/it/pagopa/ecommerce/eventdispatcher/client/NotificationsServiceClientTest.kt
@@ -1,159 +1,262 @@
 package it.pagopa.ecommerce.eventdispatcher.client
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import it.pagopa.generated.notifications.templates.ko.KoTemplate
 import it.pagopa.generated.notifications.templates.success.*
+import it.pagopa.generated.notifications.v1.ApiClient
 import it.pagopa.generated.notifications.v1.api.DefaultApi
 import it.pagopa.generated.notifications.v1.dto.NotificationEmailRequestDto
 import it.pagopa.generated.notifications.v1.dto.NotificationEmailResponseDto
-import java.nio.charset.Charset
-import java.time.ZonedDateTime
-import java.util.*
+import it.pagopa.generated.notifications.v1.dto.ProblemJsonDto
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.junit.jupiter.api.AfterAll
+import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
-import org.mockito.Mockito
 import org.mockito.kotlin.given
 import org.mockito.kotlin.mock
-import org.springframework.http.HttpHeaders
+import org.springframework.web.reactive.function.client.WebClient
 import org.springframework.web.reactive.function.client.WebClientResponseException
-import reactor.core.publisher.Mono
 import reactor.test.StepVerifier
+import java.net.URI
+import java.time.ZonedDateTime
+import java.util.*
 
 class NotificationsServiceClientTest {
 
-  private val defaultApi: DefaultApi = mock()
+    private val defaultApi: DefaultApi = mock()
 
-  private val apiKey = "apiKey"
+    private val apiClient: ApiClient = mock()
 
-  private val client = NotificationsServiceClient(defaultApi, apiKey)
+    private val webClient: WebClient = mock()
 
-  @Test
-  fun `should return email outcome`() {
-    val notificationEmailRequest =
-      NotificationEmailRequestDto()
-        .language("it-IT")
-        .subject("subject")
-        .to("foo@example.com")
-        .templateId("template-id")
-        .parameters(mapOf(Pair("param1", "value1")))
-    val expected = NotificationEmailResponseDto().outcome("OK")
-    Mockito.`when`(defaultApi.sendNotificationEmail(apiKey, notificationEmailRequest))
-      .thenReturn(Mono.just(expected))
-    StepVerifier.create(client.sendNotificationEmail(notificationEmailRequest))
-      .expectNext()
-      .verifyComplete()
-  }
+    private val bodyUriSpec: WebClient.RequestBodyUriSpec = mock()
 
-  @Test
-  fun `should return email outcome with success template`() {
-    val successTemplateRequest =
-      NotificationsServiceClient.SuccessTemplateRequest(
-        "foo@example.com",
-        "Hai pagato un avviso di pagamento PagoPA",
-        "it-IT",
-        SuccessTemplate(
-          TransactionTemplate(
-            "transactionId",
-            ZonedDateTime.now().toString(),
-            "€ 0.00",
-            PspTemplate("pspId", FeeTemplate("€ 0.00")),
-            "RRN",
-            "authorizationCode",
-            PaymentMethodTemplate("paymentInstrumentId", "paymentMethodLogo", null, false)),
-          UserTemplate(DataTemplate(null, null, null), "foo@example.com"),
-          CartTemplate(
-            listOf(
-              ItemTemplate(
-                RefNumberTemplate(RefNumberTemplate.Type.CODICE_AVVISO, "rptId"),
-                DebtorTemplate(null, null),
-                PayeeTemplate(null, null),
-                "description",
-                "€ 0.00")),
-            "€ 0.00")))
-    val request =
-      NotificationEmailRequestDto()
-        .language(successTemplateRequest.language)
-        .subject(successTemplateRequest.subject)
-        .to(successTemplateRequest.to)
-        .templateId(NotificationsServiceClient.SuccessTemplateRequest.TEMPLATE_ID)
-        .parameters(successTemplateRequest.templateParameters)
-    val expected = NotificationEmailResponseDto().outcome("OK")
-    given(defaultApi.sendNotificationEmail(apiKey, request)).willReturn(Mono.just(expected))
-    StepVerifier.create(client.sendNotificationEmail(request)).expectNext().verifyComplete()
-  }
+    private val headersSpec: WebClient.RequestHeadersSpec<*> = mock()
 
-  @Test
-  fun `should return email outcome with KO template`() {
-    val koTemplateRequest =
-      NotificationsServiceClient.KoTemplateRequest(
-        "foo@example.com",
-        "Ops! Il pagamento di € 12,00 tramite PagoPA non è riuscito",
-        "it-IT",
-        KoTemplate(
-          it.pagopa.generated.notifications.templates.ko.TransactionTemplate(
-            UUID.randomUUID().toString().uppercase(Locale.getDefault()),
-            ZonedDateTime.now().toString(),
-            "€ 12,00")))
-    val request =
-      NotificationEmailRequestDto()
-        .language(koTemplateRequest.language)
-        .subject(koTemplateRequest.subject)
-        .templateId(NotificationsServiceClient.KoTemplateRequest.TEMPLATE_ID)
-        .parameters(koTemplateRequest.templateParameters)
-    val expected = NotificationEmailResponseDto().outcome("OK")
-    given(defaultApi.sendNotificationEmail(apiKey, request)).willReturn(Mono.just(expected))
-    StepVerifier.create(client.sendNotificationEmail(request)).expectNext().verifyComplete()
-  }
+    private val apiKey = "apiKey"
 
-  @Test
-  fun `should return Mono error for exception during notificationService invocation`() {
-    val koTemplateRequest =
-      NotificationsServiceClient.KoTemplateRequest(
-        "foo@example.com",
-        "Ops! Il pagamento di € 12,00 tramite PagoPA non è riuscito",
-        "it-IT",
-        KoTemplate(
-          it.pagopa.generated.notifications.templates.ko.TransactionTemplate(
-            UUID.randomUUID().toString().uppercase(Locale.getDefault()),
-            ZonedDateTime.now().toString(),
-            "€ 12,00")))
-    val request =
-      NotificationEmailRequestDto()
-        .language(koTemplateRequest.language)
-        .subject(koTemplateRequest.subject)
-        .templateId(NotificationsServiceClient.KoTemplateRequest.TEMPLATE_ID)
-        .parameters(koTemplateRequest.templateParameters)
+    private val client = NotificationsServiceClient(defaultApi, apiKey)
 
-    given(defaultApi.sendNotificationEmail(apiKey, request))
-      .willReturn(Mono.error(RuntimeException("Error")))
-    StepVerifier.create(client.sendNotificationEmail(request))
-      .expectError(RuntimeException::class.java)
-      .verify()
-  }
+    companion object {
 
-  @Test
-  fun `should return Mono error for exception thrown during notificationService invocation`() {
-    val koTemplateRequest =
-      NotificationsServiceClient.KoTemplateRequest(
-        "foo@example.com",
-        "Ops! Il pagamento di € 12,00 tramite PagoPA non è riuscito",
-        "it-IT",
-        KoTemplate(
-          it.pagopa.generated.notifications.templates.ko.TransactionTemplate(
-            UUID.randomUUID().toString().uppercase(Locale.getDefault()),
-            ZonedDateTime.now().toString(),
-            "€ 12,00")))
-    val request =
-      NotificationEmailRequestDto()
-        .language(koTemplateRequest.language)
-        .subject(koTemplateRequest.subject)
-        .templateId(NotificationsServiceClient.KoTemplateRequest.TEMPLATE_ID)
-        .parameters(koTemplateRequest.templateParameters)
+        lateinit var mockWebServer: MockWebServer
 
-    given(defaultApi.sendNotificationEmail(apiKey, request))
-      .willThrow(
-        WebClientResponseException.create(
-          404, "Not found", HttpHeaders.EMPTY, ByteArray(0), Charset.defaultCharset()))
-    StepVerifier.create(client.sendNotificationEmail(request))
-      .expectError(RuntimeException::class.java)
-      .verify()
-  }
+        val objectMapper = ObjectMapper()
+
+        @JvmStatic
+        @BeforeAll
+        fun setup() {
+            mockWebServer = MockWebServer()
+            mockWebServer.start(8080)
+            println("Mock web server started on ${mockWebServer.hostName}:${mockWebServer.port}")
+        }
+
+        @JvmStatic
+        @AfterAll
+        fun tearDown() {
+            mockWebServer.shutdown()
+            println("Mock web stopped")
+        }
+
+    }
+
+    @Test
+    fun `should return email outcome`() {
+        val notificationEmailRequest =
+            NotificationEmailRequestDto()
+                .language("it-IT")
+                .subject("subject")
+                .to("foo@example.com")
+                .templateId("template-id")
+                .parameters(mapOf(Pair("param1", "value1")))
+        val notificationEmailResponseDto = NotificationEmailResponseDto().apply { outcome = "OK" }
+        val baseUrl = "http://${mockWebServer.hostName}:${mockWebServer.port}"
+        given(apiClient.basePath).willReturn(baseUrl)
+        given(defaultApi.apiClient).willReturn(apiClient)
+        given(apiClient.webClient).willReturn(WebClient.create(baseUrl))
+        mockWebServer.enqueue(
+            MockResponse()
+                .setBody(objectMapper.writeValueAsString(notificationEmailResponseDto))
+                .setResponseCode(200)
+                .addHeader("Content-Type", "application/json")
+        )
+        StepVerifier.create(client.sendNotificationEmail(notificationEmailRequest))
+            .expectNext(notificationEmailResponseDto)
+            .verifyComplete()
+    }
+
+    @Test
+    fun `should return email outcome with success template`() {
+        val successTemplateRequest =
+            NotificationsServiceClient.SuccessTemplateRequest(
+                "foo@example.com",
+                "Hai pagato un avviso di pagamento PagoPA",
+                "it-IT",
+                SuccessTemplate(
+                    TransactionTemplate(
+                        "transactionId",
+                        ZonedDateTime.now().toString(),
+                        "€ 0.00",
+                        PspTemplate("pspId", FeeTemplate("€ 0.00")),
+                        "RRN",
+                        "authorizationCode",
+                        PaymentMethodTemplate("paymentInstrumentId", "paymentMethodLogo", null, false)
+                    ),
+                    UserTemplate(DataTemplate(null, null, null), "foo@example.com"),
+                    CartTemplate(
+                        listOf(
+                            ItemTemplate(
+                                RefNumberTemplate(RefNumberTemplate.Type.CODICE_AVVISO, "rptId"),
+                                DebtorTemplate(null, null),
+                                PayeeTemplate(null, null),
+                                "description",
+                                "€ 0.00"
+                            )
+                        ),
+                        "€ 0.00"
+                    )
+                )
+            )
+        val request =
+            NotificationEmailRequestDto()
+                .language(successTemplateRequest.language)
+                .subject(successTemplateRequest.subject)
+                .to(successTemplateRequest.to)
+                .templateId(NotificationsServiceClient.SuccessTemplateRequest.TEMPLATE_ID)
+                .parameters(successTemplateRequest.templateParameters)
+        val notificationEmailResponseDto = NotificationEmailResponseDto().apply { outcome = "OK" }
+        val baseUrl = "http://${mockWebServer.hostName}:${mockWebServer.port}"
+        given(apiClient.basePath).willReturn(baseUrl)
+        given(defaultApi.apiClient).willReturn(apiClient)
+        given(apiClient.webClient).willReturn(WebClient.create(baseUrl))
+        mockWebServer.enqueue(
+            MockResponse()
+                .setBody(objectMapper.writeValueAsString(notificationEmailResponseDto))
+                .setResponseCode(200)
+                .addHeader("Content-Type", "application/json")
+        )
+        StepVerifier.create(client.sendNotificationEmail(request))
+            .expectNext(notificationEmailResponseDto)
+            .verifyComplete()
+    }
+
+    @Test
+    fun `should return email outcome with KO template`() {
+        val koTemplateRequest =
+            NotificationsServiceClient.KoTemplateRequest(
+                "foo@example.com",
+                "Ops! Il pagamento di € 12,00 tramite PagoPA non è riuscito",
+                "it-IT",
+                KoTemplate(
+                    it.pagopa.generated.notifications.templates.ko.TransactionTemplate(
+                        UUID.randomUUID().toString().uppercase(Locale.getDefault()),
+                        ZonedDateTime.now().toString(),
+                        "€ 12,00"
+                    )
+                )
+            )
+        val request =
+            NotificationEmailRequestDto()
+                .language(koTemplateRequest.language)
+                .subject(koTemplateRequest.subject)
+                .templateId(NotificationsServiceClient.KoTemplateRequest.TEMPLATE_ID)
+                .parameters(koTemplateRequest.templateParameters)
+        val notificationEmailResponseDto = NotificationEmailResponseDto().apply { outcome = "OK" }
+        val baseUrl = "http://${mockWebServer.hostName}:${mockWebServer.port}"
+        given(apiClient.basePath).willReturn(baseUrl)
+        given(defaultApi.apiClient).willReturn(apiClient)
+        given(apiClient.webClient).willReturn(WebClient.create(baseUrl))
+        mockWebServer.enqueue(
+            MockResponse()
+                .setBody(objectMapper.writeValueAsString(notificationEmailResponseDto))
+                .setResponseCode(200)
+                .addHeader("Content-Type", "application/json")
+        )
+        StepVerifier.create(client.sendNotificationEmail(request))
+            .expectNext(notificationEmailResponseDto)
+            .verifyComplete()
+    }
+
+    @Test
+    fun `should return Mono error for KO response received from Notifications service`() {
+        val koTemplateRequest =
+            NotificationsServiceClient.KoTemplateRequest(
+                "foo@example.com",
+                "Ops! Il pagamento di € 12,00 tramite PagoPA non è riuscito",
+                "it-IT",
+                KoTemplate(
+                    it.pagopa.generated.notifications.templates.ko.TransactionTemplate(
+                        UUID.randomUUID().toString().uppercase(Locale.getDefault()),
+                        ZonedDateTime.now().toString(),
+                        "€ 12,00"
+                    )
+                )
+            )
+        val request =
+            NotificationEmailRequestDto()
+                .language(koTemplateRequest.language)
+                .subject(koTemplateRequest.subject)
+                .templateId(NotificationsServiceClient.KoTemplateRequest.TEMPLATE_ID)
+                .parameters(koTemplateRequest.templateParameters)
+        val baseUrl = "http://${mockWebServer.hostName}:${mockWebServer.port}"
+        given(apiClient.basePath).willReturn(baseUrl)
+        given(defaultApi.apiClient).willReturn(apiClient)
+        given(apiClient.webClient).willReturn(WebClient.create(baseUrl))
+        mockWebServer.enqueue(
+            MockResponse()
+                .setBody(objectMapper.writeValueAsString(ProblemJsonDto().apply {
+                    type = URI.create("http://localhost")
+                    title = "Internal server error"
+                    status = 500
+                    detail = "Error details"
+                    instance = URI.create("http://instanceURI")
+
+                }))
+                .setResponseCode(500)
+                .addHeader("Content-Type", "text/html")
+        )
+        StepVerifier.create(client.sendNotificationEmail(request))
+            .expectError(WebClientResponseException::class.java)
+            .verify()
+    }
+
+    @Test
+    fun `should handle Notifications service HTTP 202 accepted response as OK`() {
+        val koTemplateRequest =
+            NotificationsServiceClient.KoTemplateRequest(
+                "foo@example.com",
+                "Ops! Il pagamento di € 12,00 tramite PagoPA non è riuscito",
+                "it-IT",
+                KoTemplate(
+                    it.pagopa.generated.notifications.templates.ko.TransactionTemplate(
+                        UUID.randomUUID().toString().uppercase(Locale.getDefault()),
+                        ZonedDateTime.now().toString(),
+                        "€ 12,00"
+                    )
+                )
+            )
+        val request =
+            NotificationEmailRequestDto()
+                .language(koTemplateRequest.language)
+                .subject(koTemplateRequest.subject)
+                .templateId(NotificationsServiceClient.KoTemplateRequest.TEMPLATE_ID)
+                .parameters(koTemplateRequest.templateParameters)
+        val baseUrl = "http://${mockWebServer.hostName}:${mockWebServer.port}"
+        val notificationEmailResponseDto = NotificationEmailResponseDto().apply { outcome = "OK" }
+        given(apiClient.basePath).willReturn(baseUrl)
+        given(defaultApi.apiClient).willReturn(apiClient)
+        given(apiClient.webClient).willReturn(WebClient.create(baseUrl))
+        mockWebServer.enqueue(
+            MockResponse()
+                .setBody("Accepted")
+                .setResponseCode(202)
+                .addHeader("Content-Type", "text/html")
+        )
+        StepVerifier.create(client.sendNotificationEmail(request))
+            .expectNext(notificationEmailResponseDto)
+            .verifyComplete()
+    }
+
+
 }
