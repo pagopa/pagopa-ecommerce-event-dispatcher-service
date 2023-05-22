510cea424787303811d519b176de5012363e1edc
Pietro Tota <pietro.tota@pagopa.it>
2023-05-22 16:30:55 +0200

    fix: refactoring http client

--- src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/client/NotificationsServiceClient.kt
+++ src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/client/NotificationsServiceClient.kt
@@ -4,6 +4,8 @@ import it.pagopa.generated.notifications.templates.ko.KoTemplate
 import it.pagopa.generated.notifications.templates.success.SuccessTemplate
 import it.pagopa.generated.notifications.v1.api.DefaultApi
 import it.pagopa.generated.notifications.v1.dto.NotificationEmailRequestDto
+import it.pagopa.generated.notifications.v1.dto.NotificationEmailResponseDto
+import kotlinx.coroutines.reactor.mono
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
@@ -24,33 +26,50 @@ class NotificationsServiceClient(
 
   val logger: Logger = LoggerFactory.getLogger(NotificationsServiceClient::class.java)
 
-  fun sendNotificationEmail(notificationEmailRequestDto: NotificationEmailRequestDto): Mono<Void> {
-    val response: Mono<Void> =
-      try {
-        notificationsServiceApi
-          .sendNotificationEmailWithHttpInfo(
-            notificationsServiceApiKey, notificationEmailRequestDto)
-          .flatMap {
-            when (it.statusCode) {
-              HttpStatus.OK -> Mono.empty()
-              HttpStatus.ACCEPTED -> Mono.empty()
-              else ->
-                Mono.error(RuntimeException("Invalid notifications service response received! $it"))
+  fun sendNotificationEmail(
+    notificationEmailRequestDto: NotificationEmailRequestDto
+  ): Mono<NotificationEmailResponseDto> {
+    val transactionId = getTransactionIdFromParameters(notificationEmailRequestDto.parameters)
+    return notificationsServiceApi.apiClient.webClient
+      .post()
+      .uri("${notificationsServiceApi.apiClient.basePath}/emails")
+      .header("ocp-apim-subscription-key", notificationsServiceApiKey)
+      .body(mono { notificationEmailRequestDto }, NotificationEmailRequestDto::class.java)
+      .exchangeToMono { response ->
+        logger.info(
+          "Notification for transaction with id $transactionId sent, notifications-service code ${response.statusCode()}")
+
+        when (response.statusCode()) {
+          HttpStatus.OK -> {
+            logger.info("Mail sent successfully for transaction id: $transactionId")
+            response.bodyToMono(NotificationEmailResponseDto::class.java)
+          }
+          HttpStatus.ACCEPTED -> {
+            logger.info(
+              "Mail sending accepted for transaction id: $transactionId, retries will be attempted by notifications-service module")
+            response.toBodilessEntity().flatMap {
+              Mono.just(NotificationEmailResponseDto().apply { outcome = "OK" })
             }
           }
-      } catch (e: Exception) {
-        Mono.error(e)
+          else -> response.createException().flatMap { error -> Mono.error(error) }
+        }
       }
-    return response
       .doOnError(WebClientResponseException::class.java) { e: WebClientResponseException ->
         logger.error(
-          "Got bad response from notifications-service [HTTP {}]: {}",
+          "Error sending email for transaction id: {}. Got bad response from notifications-service [HTTP {}]: {}",
+          transactionId,
           e.statusCode,
           e.responseBodyAsString)
       }
       .doOnError { e: Throwable -> logger.error(e.toString()) }
-      .then()
   }
+
+  fun getTransactionIdFromParameters(parameters: Any): String? =
+    when (parameters) {
+      is KoTemplate -> parameters.transaction.id
+      is SuccessTemplate -> parameters.transaction.id
+      else -> "N/A"
+    }
 
   data class SuccessTemplateRequest(
     val to: String,
