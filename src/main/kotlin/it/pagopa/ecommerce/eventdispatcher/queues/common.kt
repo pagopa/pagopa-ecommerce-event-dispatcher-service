16cef787ba36bcbc405105cfcc7efd540e004eb7
Fernando-Granato <117268775+Fernando-Granato@users.noreply.github.com>
2023-09-06 09:34:49 +0200

    feat(pgs status): CHK-1804 fix created status from pgs (#141)
    * fix(pgs refund): modified logic for refund status CRATED received from pgs
    
    * fix(log):fixed log
    
    * fix(tests):resolved problem into test

--- src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/queues/common.kt
+++ src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/queues/common.kt
@@ -236,9 +236,10 @@ fun handleVposRefundResponse(
       updateTransactionToRefunded(
         transaction, transactionsEventStoreRepository, transactionsViewRepository)
     }
-    StatusEnum.DENIED -> {
+    StatusEnum.DENIED,
+    StatusEnum.CREATED -> {
       logger.info(
-        "Refund for transaction with id: [${transaction.transactionId.value()}] denied! No more attempts will be performed")
+        "Refund for transaction with id: [${transaction.transactionId.value()}] denied with pgs response status: [${refundResponse.status}]! No more attempts will be performed")
       updateTransactionToRefundError(
         transaction, transactionsEventStoreRepository, transactionsViewRepository)
     }
@@ -265,9 +266,10 @@ fun handleXpayRefundResponse(
       updateTransactionToRefunded(
         transaction, transactionsEventStoreRepository, transactionsViewRepository)
     }
-    XPayRefundResponse200Dto.StatusEnum.DENIED -> {
+    XPayRefundResponse200Dto.StatusEnum.DENIED,
+    XPayRefundResponse200Dto.StatusEnum.CREATED -> {
       logger.info(
-        "Refund for transaction with id: [${transaction.transactionId.value()}] denied! No more attempts will be performed")
+        "Refund for transaction with id: [${transaction.transactionId.value()}] denied with pgs response status: [${refundResponse.status}]!! No more attempts will be performed")
       updateTransactionToRefundError(
         transaction, transactionsEventStoreRepository, transactionsViewRepository)
     }
