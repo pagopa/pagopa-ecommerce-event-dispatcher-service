f41dbb740f7fb08eb8462f8ff71da901efa0e414
Pietro Tota <pietro.tota@pagopa.it>
2023-09-25 12:30:25 +0200

    fix(lint): spotless

--- src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/config/QueuesConsumerConfig.kt
+++ src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/config/QueuesConsumerConfig.kt
@@ -17,106 +17,98 @@ import org.springframework.integration.annotation.Poller
 @Configuration
 class QueuesConsumerConfig {
 
-    @Bean
-    @InboundChannelAdapter(
-        channel = "transactionclosureschannel",
-        poller = [Poller(fixedDelay = "1000", maxMessagesPerPoll = "10")]
-    )
-    fun storageQueueClosuresMessageSource(
-        storageQueueTemplate: StorageQueueTemplate,
-        @Value("\${azurestorage.queues.transactionclosepayment.name}") queueNameClosureEvents: String
-    ): StorageQueueMessageSource {
-        return StorageQueueMessageSource(queueNameClosureEvents, storageQueueTemplate)
-    }
+  @Bean
+  @InboundChannelAdapter(
+    channel = "transactionclosureschannel",
+    poller = [Poller(fixedDelay = "1000", maxMessagesPerPoll = "10")])
+  fun storageQueueClosuresMessageSource(
+    storageQueueTemplate: StorageQueueTemplate,
+    @Value("\${azurestorage.queues.transactionclosepayment.name}") queueNameClosureEvents: String
+  ): StorageQueueMessageSource {
+    return StorageQueueMessageSource(queueNameClosureEvents, storageQueueTemplate)
+  }
 
-    @Bean
-    @InboundChannelAdapter(
-        channel = "transactionretryclosureschannel",
-        poller = [Poller(fixedDelay = "1000", maxMessagesPerPoll = "10")]
-    )
-    fun storageQueueRetryClosuresMessageSource(
-        storageQueueTemplate: StorageQueueTemplate,
-        @Value("\${azurestorage.queues.transactionclosepaymentretry.name}")
-        queueNameClosureEvents: String
-    ): StorageQueueMessageSource {
-        return StorageQueueMessageSource(queueNameClosureEvents, storageQueueTemplate)
-    }
+  @Bean
+  @InboundChannelAdapter(
+    channel = "transactionretryclosureschannel",
+    poller = [Poller(fixedDelay = "1000", maxMessagesPerPoll = "10")])
+  fun storageQueueRetryClosuresMessageSource(
+    storageQueueTemplate: StorageQueueTemplate,
+    @Value("\${azurestorage.queues.transactionclosepaymentretry.name}")
+    queueNameClosureEvents: String
+  ): StorageQueueMessageSource {
+    return StorageQueueMessageSource(queueNameClosureEvents, storageQueueTemplate)
+  }
 
-    @Bean
-    @InboundChannelAdapter(
-        channel = "transactionexpiredchannel",
-        poller = [Poller(fixedDelay = "1000", maxMessagesPerPoll = "10")]
-    )
-    fun storageQueueExpirationsMessageSource(
-        storageQueueTemplate: StorageQueueTemplate,
-        @Value("\${azurestorage.queues.transactionexpiration.name}") queueNameClosureEvents: String
-    ): StorageQueueMessageSource {
-        return StorageQueueMessageSource(queueNameClosureEvents, storageQueueTemplate)
-    }
+  @Bean
+  @InboundChannelAdapter(
+    channel = "transactionexpiredchannel",
+    poller = [Poller(fixedDelay = "1000", maxMessagesPerPoll = "10")])
+  fun storageQueueExpirationsMessageSource(
+    storageQueueTemplate: StorageQueueTemplate,
+    @Value("\${azurestorage.queues.transactionexpiration.name}") queueNameClosureEvents: String
+  ): StorageQueueMessageSource {
+    return StorageQueueMessageSource(queueNameClosureEvents, storageQueueTemplate)
+  }
 
-    @Bean
-    @InboundChannelAdapter(
-        channel = "transactionrefundretrychannel",
-        poller = [Poller(fixedDelay = "1000", maxMessagesPerPoll = "10")]
-    )
-    fun storageQueueRefundRetryMessageSource(
-        storageQueueTemplate: StorageQueueTemplate,
-        @Value("\${azurestorage.queues.transactionrefundretry.name}") queueNameRefundRetryEvents: String
-    ): StorageQueueMessageSource {
-        return StorageQueueMessageSource(queueNameRefundRetryEvents, storageQueueTemplate)
-    }
+  @Bean
+  @InboundChannelAdapter(
+    channel = "transactionrefundretrychannel",
+    poller = [Poller(fixedDelay = "1000", maxMessagesPerPoll = "10")])
+  fun storageQueueRefundRetryMessageSource(
+    storageQueueTemplate: StorageQueueTemplate,
+    @Value("\${azurestorage.queues.transactionrefundretry.name}") queueNameRefundRetryEvents: String
+  ): StorageQueueMessageSource {
+    return StorageQueueMessageSource(queueNameRefundRetryEvents, storageQueueTemplate)
+  }
 
-    @Bean
-    @InboundChannelAdapter(
-        channel = "transactionsrefundchannel",
-        poller = [Poller(fixedDelay = "1000", maxMessagesPerPoll = "10")]
-    )
-    fun storageQueueRefundMessageSource(
-        storageQueueTemplate: StorageQueueTemplate,
-        @Value("\${azurestorage.queues.transactionsrefund.name}") queueNameRefundRetryEvents: String
-    ): StorageQueueMessageSource {
-        return StorageQueueMessageSource(queueNameRefundRetryEvents, storageQueueTemplate)
-    }
+  @Bean
+  @InboundChannelAdapter(
+    channel = "transactionsrefundchannel",
+    poller = [Poller(fixedDelay = "1000", maxMessagesPerPoll = "10")])
+  fun storageQueueRefundMessageSource(
+    storageQueueTemplate: StorageQueueTemplate,
+    @Value("\${azurestorage.queues.transactionsrefund.name}") queueNameRefundRetryEvents: String
+  ): StorageQueueMessageSource {
+    return StorageQueueMessageSource(queueNameRefundRetryEvents, storageQueueTemplate)
+  }
 
-    @Bean
-    @InboundChannelAdapter(
-        channel = "transactionretrynotificationschannel",
-        poller = [Poller(fixedDelay = "1000", maxMessagesPerPoll = "10")]
-    )
-    fun storageQueueRetryNotificationsMessageSource(
-        storageQueueTemplate: StorageQueueTemplate,
-        @Value("\${azurestorage.queues.transactionnotificationretry.name}")
-        queueNameClosureEvents: String
-    ): StorageQueueMessageSource {
-        return StorageQueueMessageSource(queueNameClosureEvents, storageQueueTemplate)
-    }
+  @Bean
+  @InboundChannelAdapter(
+    channel = "transactionretrynotificationschannel",
+    poller = [Poller(fixedDelay = "1000", maxMessagesPerPoll = "10")])
+  fun storageQueueRetryNotificationsMessageSource(
+    storageQueueTemplate: StorageQueueTemplate,
+    @Value("\${azurestorage.queues.transactionnotificationretry.name}")
+    queueNameClosureEvents: String
+  ): StorageQueueMessageSource {
+    return StorageQueueMessageSource(queueNameClosureEvents, storageQueueTemplate)
+  }
 
-    @Bean
-    @InboundChannelAdapter(
-        channel = "transactionnotificationschannel",
-        poller = [Poller(fixedDelay = "1000", maxMessagesPerPoll = "10")]
-    )
-    fun storageQueueNotificationsMessageSource(
-        storageQueueTemplate: StorageQueueTemplate,
-        @Value("\${azurestorage.queues.transactionnotification.name}") queueNameClosureEvents: String
-    ): StorageQueueMessageSource {
-        return StorageQueueMessageSource(queueNameClosureEvents, storageQueueTemplate)
-    }
+  @Bean
+  @InboundChannelAdapter(
+    channel = "transactionnotificationschannel",
+    poller = [Poller(fixedDelay = "1000", maxMessagesPerPoll = "10")])
+  fun storageQueueNotificationsMessageSource(
+    storageQueueTemplate: StorageQueueTemplate,
+    @Value("\${azurestorage.queues.transactionnotification.name}") queueNameClosureEvents: String
+  ): StorageQueueMessageSource {
+    return StorageQueueMessageSource(queueNameClosureEvents, storageQueueTemplate)
+  }
 
-    @Bean
-    fun strictSerializerProviderV1(): StrictJsonSerializerProvider =
-        StrictJsonSerializerProvider()
-            .addMixIn(QueueEvent::class.java, QueueEventMixInEventCodeFieldDiscriminator::class.java).addMixIn(
-                it.pagopa.ecommerce.commons.documents.v1.TransactionEvent::class.java,
-                TransactionEventMixInEventCodeFieldDiscriminator::class.java
-            )
+  @Bean
+  fun strictSerializerProviderV1(): StrictJsonSerializerProvider =
+    StrictJsonSerializerProvider()
+      .addMixIn(QueueEvent::class.java, QueueEventMixInEventCodeFieldDiscriminator::class.java)
+      .addMixIn(
+        it.pagopa.ecommerce.commons.documents.v1.TransactionEvent::class.java,
+        TransactionEventMixInEventCodeFieldDiscriminator::class.java)
 
-    @Bean
-    fun strictSerializerProviderV2(): StrictJsonSerializerProvider =
-        StrictJsonSerializerProvider()
-            .addMixIn(QueueEvent::class.java, QueueEventMixInClassFieldDiscriminator::class.java)
-            .addMixIn(
-                it.pagopa.ecommerce.commons.documents.v2.TransactionEvent::class.java,
-                TransactionEventMixInClassFieldDiscriminator::class.java
-            )
+  @Bean
+  fun strictSerializerProviderV2(): StrictJsonSerializerProvider =
+    StrictJsonSerializerProvider()
+      .addMixIn(QueueEvent::class.java, QueueEventMixInClassFieldDiscriminator::class.java)
+      .addMixIn(
+        it.pagopa.ecommerce.commons.documents.v2.TransactionEvent::class.java,
+        TransactionEventMixInClassFieldDiscriminator::class.java)
 }
