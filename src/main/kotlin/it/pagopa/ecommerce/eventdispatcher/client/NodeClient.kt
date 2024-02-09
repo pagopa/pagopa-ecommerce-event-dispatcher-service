3eea8e52d86e8bebc2e125daf34d85771c230f6e
Pietro Tota <pietro.tota@pagopa.it>
2024-02-09 17:21:14 +0100

    fix: extract node close payment error response

--- src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/client/NodeClient.kt
+++ src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/client/NodeClient.kt
@@ -5,13 +5,16 @@ import it.pagopa.generated.ecommerce.nodo.v2.api.NodoApi
 import it.pagopa.generated.ecommerce.nodo.v2.dto.ClosePaymentRequestV2Dto
 import it.pagopa.generated.ecommerce.nodo.v2.dto.ClosePaymentResponseDto
 import it.pagopa.generated.ecommerce.nodo.v2.dto.ErrorDto
-import kotlinx.coroutines.reactor.awaitSingle
 import kotlinx.coroutines.reactor.mono
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
-import org.springframework.web.reactive.function.client.WebClientResponseException
+import org.springframework.web.reactive.function.client.ClientResponse
+import org.springframework.web.server.ResponseStatusException
 import reactor.core.publisher.Mono
 
 @Component
@@ -29,27 +32,43 @@ class NodeClient(
   suspend fun closePayment(
     closePaymentRequest: ClosePaymentRequestV2Dto
   ): Mono<ClosePaymentResponseDto> {
-    return mono {
-      try {
-        return@mono nodeApi
-          .closePaymentV2(closePaymentRequest, ecommerceClientId)
-          .doOnNext {
-            logger.info(
-              "Nodo close payment response for transactionId: [{}], received outcome: [{}]",
-              closePaymentRequest.transactionId,
-              it.outcome)
+    logger.info(
+      "Requested closePaymentV2 for transactionId [{}]: paymentTokens {} - outcome: {}",
+      closePaymentRequest.transactionId,
+      closePaymentRequest.paymentTokens,
+      closePaymentRequest.outcome.value)
+    return nodeApi.apiClient.webClient
+      .post()
+      .uri { it.path("/closepayment").queryParam("clientId", ecommerceClientId).build() }
+      .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+      .body(mono { closePaymentRequest }, ClosePaymentRequestV2Dto::class.java)
+      .retrieve()
+      .onStatus(
+        { obj: HttpStatus -> obj.isError },
+        { clientResponse: ClientResponse ->
+          clientResponse.bodyToMono(String::class.java).switchIfEmpty(Mono.just("N/A")).flatMap {
+            errorResponseBodyAsString: String ->
+            Mono.error(
+              ResponseStatusException(clientResponse.statusCode(), errorResponseBodyAsString))
           }
-          .awaitSingle()
-      } catch (exception: Exception) {
+        })
+      .bodyToMono(ClosePaymentResponseDto::class.java)
+      .doOnSuccess { closePaymentResponse: ClosePaymentResponseDto ->
+        logger.info(
+          "Received closePaymentV2 response for transactionId [{}]: paymentTokens {} - outcome: {}",
+          closePaymentRequest.transactionId,
+          closePaymentRequest.paymentTokens,
+          closePaymentResponse.outcome)
+      }
+      .onErrorMap { exception ->
         logger.error(
-          "Error performing Nodo close payment for transactionId: [${closePaymentRequest.transactionId}]",
+          "Received closePaymentV2 Response Status Error for transactionId [${closePaymentRequest.transactionId}]",
           exception)
-        if (exception is WebClientResponseException) {
+        if (exception is ResponseStatusException) {
           throw ClosePaymentErrorResponseException(
-            exception.statusCode,
+            exception.status,
             runCatching {
-                nodeApi.apiClient.objectMapper.readValue(
-                  exception.responseBodyAsString, ErrorDto::class.java)
+                nodeApi.apiClient.objectMapper.readValue(exception.reason, ErrorDto::class.java)
               }
               .onFailure {
                 logger.error("Error parsing Nodo close payment error response body", it)
@@ -59,6 +78,5 @@ class NodeClient(
           throw ClosePaymentErrorResponseException(null, null)
         }
       }
-    }
   }
 }
