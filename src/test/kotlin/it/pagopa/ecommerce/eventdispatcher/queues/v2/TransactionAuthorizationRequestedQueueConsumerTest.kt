1455190e50f12ad7a778edd30caa724a2083b2c6
Pietro Tota <pietro.tota@pagopa.it>
2024-02-16 11:47:55 +0100

    fix: add test to cover ko auth status response

--- src/test/kotlin/it/pagopa/ecommerce/eventdispatcher/queues/v2/TransactionAuthorizationRequestedQueueConsumerTest.kt
+++ src/test/kotlin/it/pagopa/ecommerce/eventdispatcher/queues/v2/TransactionAuthorizationRequestedQueueConsumerTest.kt
@@ -82,7 +82,7 @@ class TransactionAuthorizationRequestedQueueConsumerTest {
 
   @ParameterizedTest
   @MethodSource("Recover transaction status timestamp method source")
-  fun `Should recover transaction authorization status calling NPG get state`(
+  fun `Should recover transaction authorization status calling NPG get state for OK auth status`(
     receivedOperationTime: String,
     expectedOperationTime: OffsetDateTime
   ) {
@@ -128,6 +128,74 @@ class TransactionAuthorizationRequestedQueueConsumerTest {
             this.rrn = rrn
           }
         timestampOperation = expectedOperationTime
+      }
+    given(
+        transactionsEventStoreRepository.findByTransactionIdOrderByCreationDateAsc(
+          transactionId.value()))
+      .willReturn(Flux.fromIterable(events))
+    given(npgStateService.getStateNpg(any(), any(), any(), any()))
+      .willReturn(mono { npgStateResponse })
+    given(transactionsServiceClient.patchAuthRequest(any(), any()))
+      .willReturn(mono { TransactionInfoDto() })
+    given(checkpointer.success()).willReturn(Mono.empty())
+
+    // Test
+    StepVerifier.create(
+        transactionAuthorizationRequestedQueueConsumer.messageReceiver(
+          QueueEvent(transactionAuthorizationRequestedEvent, TracingInfoTest.MOCK_TRACING_INFO),
+          checkpointer))
+      .expectNext(Unit)
+      .verifyComplete()
+    // assertions
+    verify(npgStateService, times(1))
+      .getStateNpg(transactionId.uuid, expectedGetStateSessionId, PSP_ID, NPG_CORRELATION_ID)
+    verify(transactionsServiceClient, times(1))
+      .patchAuthRequest(transactionId, expectedPatchAuthRequest)
+    verify(deadLetterTracedQueueAsyncClient, times(0))
+      .sendAndTraceDeadLetterQueueEvent(any(), any())
+  }
+
+  @Test
+  fun `Should recover transaction authorization status calling NPG get state for KO auth status`() {
+    // pre-conditions
+    val transactionActivatedEvent = transactionActivateEvent(npgTransactionGatewayActivationData())
+    val transactionAuthorizationRequestedEvent =
+      transactionAuthorizationRequestedEvent(
+        TransactionAuthorizationRequestData.PaymentGateway.NPG,
+        npgTransactionGatewayAuthorizationRequestedData())
+    val transactionId = TransactionId(TRANSACTION_ID)
+    val events: List<TransactionEvent<Any>> =
+      listOf(
+        transactionActivatedEvent as TransactionEvent<Any>,
+        transactionAuthorizationRequestedEvent as TransactionEvent<Any>)
+    val operationId = "operationId"
+    val orderId = "orderId"
+    val paymentEndToEndId = "paymentEndToEndId"
+    val npgStateResponse =
+      StateResponseDto()
+        .state(WorkflowStateDto.PAYMENT_COMPLETE)
+        .operation(
+          OperationDto()
+            .operationId(operationId)
+            .orderId(orderId)
+            .operationResult(OperationResultDto.DECLINED)
+            .paymentEndToEndId(paymentEndToEndId)
+            .operationTime("2024-01-01T00:00:00"))
+    val expectedGetStateSessionId = NPG_CONFIRM_PAYMENT_SESSION_ID
+    val expectedPatchAuthRequest =
+      UpdateAuthorizationRequestDto().apply {
+        outcomeGateway =
+          UpdateAuthorizationRequestOutcomeGatewayDto().apply {
+            this.paymentGatewayType = "NPG"
+            this.operationResult =
+              UpdateAuthorizationRequestOutcomeGatewayDto.OperationResultEnum.DECLINED
+            this.orderId = orderId
+            this.operationId = operationId
+            this.authorizationCode = null
+            this.paymentEndToEndId = paymentEndToEndId
+            this.rrn = null
+          }
+        timestampOperation = OffsetDateTime.parse("2024-01-01T00:00:00+01:00")
       }
     given(
         transactionsEventStoreRepository.findByTransactionIdOrderByCreationDateAsc(
