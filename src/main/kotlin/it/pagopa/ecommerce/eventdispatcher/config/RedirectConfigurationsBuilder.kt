058661284125e0b8389c8e13de70d982b27c6a80
Pietro Tota <pietro.tota@pagopa.it>
2024-03-21 16:22:29 +0100

    feat: add redirect api client configuration

---
+++ src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/config/RedirectConfigurationsBuilder.kt
@@ -0,0 +1,54 @@
+package it.pagopa.ecommerce.eventdispatcher.config
+
+import it.pagopa.ecommerce.commons.exceptions.RedirectConfigurationException
+import it.pagopa.ecommerce.commons.exceptions.RedirectConfigurationType
+import java.net.URI
+import java.util.function.Predicate
+import java.util.stream.Collectors
+import lombok.extern.slf4j.Slf4j
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+/**
+ * Configuration class used to read all the PSP configurations that will be used during redirect
+ * transaction
+ */
+@Configuration
+@Slf4j
+class RedirectConfigurationsBuilder {
+  /**
+   * Create a Map &lt String,URI &gt that will associate, to every handled PSP, the backend URI to
+   * be used to perform Redirect payment flow api call
+   *
+   * @param paymentTypeCodeList
+   * - set of all redirect payment type codes to be handled flow
+   * @param pspUrlMapping
+   * - configuration parameter that contains PSP to URI mapping
+   * @return a configuration map for every PSPs
+   */
+  @Bean
+  fun redirectBeApiCallUriMap(
+    @Value("\${redirect.paymentTypeCodeList}") paymentTypeCodeList: Set<String>,
+    @Value("#{\${redirect.pspUrlMapping}}") pspUrlMapping: Map<String, String>
+  ): Map<String, URI> {
+    val redirectUriMap: MutableMap<String, URI> = HashMap()
+    // URI.create throws IllegalArgumentException that will prevent module load for
+    // invalid PSP URI configuration
+    pspUrlMapping.forEach { (pspId: String, uri: String?) ->
+      redirectUriMap[pspId] = URI.create(uri)
+    }
+    val missingKeys =
+      paymentTypeCodeList
+        .stream()
+        .filter(Predicate.not { key: String -> redirectUriMap.containsKey(key) })
+        .collect(Collectors.toSet())
+    if (missingKeys.isNotEmpty()) {
+      throw RedirectConfigurationException(
+        "Misconfigured redirect.pspUrlMapping, the following redirect payment type code b.e. URIs are not configured: %s".format(
+          missingKeys),
+        RedirectConfigurationType.BACKEND_URLS)
+    }
+    return redirectUriMap
+  }
+}
---
+++ src/test/kotlin/it/pagopa/ecommerce/eventdispatcher/config/RedirectConfigurationsBuilderTest.kt
@@ -0,0 +1,54 @@
+package it.pagopa.ecommerce.eventdispatcher.config
+
+import it.pagopa.ecommerce.commons.exceptions.RedirectConfigurationException
+import java.net.URI
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+
+internal class RedirectConfigurationsBuilderTest {
+  private val checkoutRedirectConfigurationsBuilder = RedirectConfigurationsBuilder()
+  private val pspToHandle = setOf("key1", "key2", "key3")
+  private val pspUriMap =
+    java.util.Map.of(
+      "key1",
+      "http://localhost/key1/redirectionUrl",
+      "key2",
+      "http://localhost/key2/redirectionUrl",
+      "key3",
+      "http://localhost/key3/redirectionUrl")
+
+  @ParameterizedTest
+  @ValueSource(strings = ["key1", "key2", "key3"])
+  fun shouldBuildPspBackendUriMapSuccessfully(pspId: String) {
+    val mapping: Map<String, URI> = assertDoesNotThrow {
+      checkoutRedirectConfigurationsBuilder.redirectBeApiCallUriMap(pspToHandle, pspUriMap)
+    }
+    assertEquals("http://localhost/%s/redirectionUrl".formatted(pspId), mapping[pspId].toString())
+  }
+
+  @Test
+  fun shouldThrowExceptionBuildingBackendUriMapForMissingApiKey() {
+    val missingKeyPspMap: MutableMap<String, String> = HashMap(pspUriMap)
+    missingKeyPspMap.remove("key1")
+    val e: RedirectConfigurationException =
+      assertThrows(RedirectConfigurationException::class.java) {
+        checkoutRedirectConfigurationsBuilder.redirectBeApiCallUriMap(pspToHandle, missingKeyPspMap)
+      }
+    assertEquals(
+      "Error parsing Redirect PSP BACKEND_URLS configuration, cause: Misconfigured redirect.pspUrlMapping, the following redirect payment type code b.e. URIs are not configured: [key1]",
+      e.message)
+  }
+
+  @Test
+  fun shouldThrowExceptionBuildingBackendUriMapForWrongUri() {
+    val missingKeyPspMap: MutableMap<String, String> = HashMap(pspUriMap)
+    missingKeyPspMap["key1"] = "http:\\\\localhost"
+    assertThrows(IllegalArgumentException::class.java) {
+      checkoutRedirectConfigurationsBuilder.redirectBeApiCallUriMap(pspToHandle, missingKeyPspMap)
+    }
+  }
+}
