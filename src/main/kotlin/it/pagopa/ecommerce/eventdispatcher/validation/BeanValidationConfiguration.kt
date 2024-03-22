50b304643c93f2d833bc4c64b4893ba25e4f0153
Pietro Tota <pietro.tota@pagopa.it>
2024-03-22 11:58:46 +0100

    fix: junit tests

--- src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/validation/BeanValidationConfiguration.kt
+++ src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/validation/BeanValidationConfiguration.kt
@@ -1,6 +1,5 @@
 package it.pagopa.ecommerce.eventdispatcher.validation
 
-import it.pagopa.ecommerce.payment.requests.validation.CustomLocalValidatorFactoryBean
 import org.springframework.beans.factory.config.BeanDefinition
 import org.springframework.boot.validation.MessageInterpolatorFactory
 import org.springframework.context.annotation.Bean
@@ -11,22 +10,22 @@ import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean
 
 @Configuration(proxyBeanMethods = false)
 class BeanValidationConfiguration {
-  /**
-   * This bean definition is part of the workaround for a bug in hibernate-validation.
-   *
-   * It replaces the default validator factory bean with ours that uses the customized parameter
-   * name discoverer.
-   *
-   * See:
-   * * Spring issue: https://github.com/spring-projects/spring-framework/issues/23499
-   * * Hibernate issue: https://hibernate.atlassian.net/browse/HV-1638
-   */
-  @Primary
-  @Bean
-  @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
-  fun defaultValidator(): LocalValidatorFactoryBean {
-    val factoryBean = CustomLocalValidatorFactoryBean()
-    factoryBean.messageInterpolator = MessageInterpolatorFactory().getObject()
-    return factoryBean
-  }
+    /**
+     * This bean definition is part of the workaround for a bug in hibernate-validation.
+     *
+     * It replaces the default validator factory bean with ours that uses the customized parameter
+     * name discoverer.
+     *
+     * See:
+     * * Spring issue: https://github.com/spring-projects/spring-framework/issues/23499
+     * * Hibernate issue: https://hibernate.atlassian.net/browse/HV-1638
+     */
+    @Primary
+    @Bean
+    @Role(BeanDefinition.ROLE_INFRASTRUCTURE)
+    fun defaultValidator(): LocalValidatorFactoryBean {
+        val factoryBean = CustomLocalValidatorFactoryBean()
+        factoryBean.messageInterpolator = MessageInterpolatorFactory().getObject()
+        return factoryBean
+    }
 }
