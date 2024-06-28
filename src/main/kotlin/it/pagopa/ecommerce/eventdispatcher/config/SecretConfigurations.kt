5d777dd5eb09986b09c859099ca087fddbb7962b
CianoDanilo <danilo.ciano@pagopa.it>
2024-06-28 11:54:13 +0200

    feat(decrypt): add wallet-session api key to tokenize user fiscal code
    * add new kv api key
    
    * add new decrypt method to ConfidentialDataUtils to handle fiscal code decrypting with a different api key
    
    * rename kv api key
    
    * feat(decrypt): change vars/properties/beans with shared prefix to have wallet session prefix

--- src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/config/SecretConfigurations.kt
+++ src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/config/SecretConfigurations.kt
@@ -3,14 +3,15 @@ package it.pagopa.ecommerce.eventdispatcher.config
 import it.pagopa.ecommerce.commons.utils.ConfidentialDataManager
 import it.pagopa.generated.pdv.v1.ApiClient
 import it.pagopa.generated.pdv.v1.api.TokenApi
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class SecretConfigurations {
-  @Bean
-  fun personalDataVaultApiClient(
+  @Bean("eCommercePersonalDataVaultApi")
+  fun eCommercePersonalDataVaultApiClient(
     @Value("\${confidentialDataManager.personalDataVault.apiKey}") personalDataVaultApiKey: String,
     @Value("\${confidentialDataManager.personalDataVault.apiBasePath}") apiBasePath: String
   ): TokenApi {
@@ -20,8 +21,29 @@ class SecretConfigurations {
     return TokenApi(pdvApiClient)
   }
 
-  @Bean
-  fun emailConfidentialDataManager(personalDataVaultApi: TokenApi): ConfidentialDataManager {
+  @Bean("walletSessionPersonalDataVaultApi")
+  fun walletSessionPersonalDataVaultApiClient(
+    @Value("\${confidentialDataManager.walletSessionPersonalDataVault.apiKey}")
+    personalDataVaultApiKey: String,
+    @Value("\${confidentialDataManager.personalDataVault.apiBasePath}") apiBasePath: String
+  ): TokenApi {
+    val pdvApiClient = ApiClient()
+    pdvApiClient.setApiKey(personalDataVaultApiKey)
+    pdvApiClient.setBasePath(apiBasePath)
+    return TokenApi(pdvApiClient)
+  }
+
+  @Bean("eCommerceConfidentialDataManager")
+  fun eCommerceConfidentialDataManager(
+    @Qualifier("eCommercePersonalDataVaultApi") personalDataVaultApi: TokenApi
+  ): ConfidentialDataManager {
+    return ConfidentialDataManager(personalDataVaultApi)
+  }
+
+  @Bean("walletSessionConfidentialDataManager")
+  fun walletSessionConfidentialDataManager(
+    @Qualifier("walletSessionPersonalDataVaultApi") personalDataVaultApi: TokenApi
+  ): ConfidentialDataManager {
     return ConfidentialDataManager(personalDataVaultApi)
   }
 }
