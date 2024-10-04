6371846c67ec088fdd3309386121351dc681e80d
Gianluca Ciuffa <gianlucaciuffa@MBP-di-Gianluca.homenet.telecomitalia.it>
2024-10-04 12:59:01 +0200

    Revert "remove if-else inside reactive chain"
    This reverts commit c1e5ab1f7e3f271f8cbc5299467ac60f7e532c03.

--- src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/queues/v2/helpers/AuthorizationRequestedHelper.kt
+++ src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/queues/v2/helpers/AuthorizationRequestedHelper.kt
@@ -111,21 +111,24 @@ class AuthorizationRequestedHelper(
 
     val authorizationRequestedPipeline =
       transaction
-        .filter { baseTransactionWithRequestedAuthorization ->
-          saveLastUsage && isAuthenticatedTransaction(baseTransactionWithRequestedAuthorization)
+        .flatMap { baseTransactionWithRequestedAuthorization ->
+          if (saveLastUsage &&
+            isAuthenticatedTransaction(baseTransactionWithRequestedAuthorization)) {
+            userStatsServiceClient
+              .saveLastUsage(
+                UUID.fromString(
+                  baseTransactionWithRequestedAuthorization.transactionActivatedData.userId!!),
+                buildUserLastPaymentMethodData(
+                  baseTransactionWithRequestedAuthorization, creationDate))
+              .onErrorResume {
+                logger.error("Exception while saving last payment method used", it)
+                mono {}
+              }
+              .thenReturn(baseTransactionWithRequestedAuthorization)
+          } else {
+            mono { baseTransactionWithRequestedAuthorization }
+          }
         }
-        .flatMap {
-          userStatsServiceClient
-            .saveLastUsage(
-              UUID.fromString(it.transactionActivatedData.userId!!),
-              buildUserLastPaymentMethodData(it, creationDate))
-            .onErrorResume {
-              logger.error("Exception while saving last payment method used", it)
-              mono {}
-            }
-            .thenReturn(it)
-        }
-        .switchIfEmpty { transaction }
         .filter {
           it.transactionAuthorizationRequestData.paymentGateway ==
             TransactionAuthorizationRequestData.PaymentGateway.NPG
