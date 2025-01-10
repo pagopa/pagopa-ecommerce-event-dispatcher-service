d3a57d9fb526e3c672caddd03939047f7f53793f
Pietro Tota <pietro.tota@pagopa.it>
2025-01-10 14:23:40 +0100

    chore: add warmup logging

--- src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/EventDispatcherApplication.kt
+++ src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/EventDispatcherApplication.kt
@@ -1,5 +1,6 @@
 package it.pagopa.ecommerce.eventdispatcher
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
@@ -10,10 +11,13 @@ import org.springframework.integration.config.EnableIntegration
 @EnableIntegration
 class EventDispatcherApplication(private val applicationContext: ApplicationContext) :
   CommandLineRunner {
+  private val logger = LoggerFactory.getLogger(EventDispatcherApplication::class.java)
   override fun run(vararg args: String?) {
+    logger.info("START performing bean warmup")
     applicationContext.beanDefinitionNames.forEach { beanName ->
       applicationContext.getBean(beanName)
     }
+    logger.info("END performing bean warmup")
   }
 }
 
