6e59999d848366d91c68a00fe7e3a455222bb04a
e.serrao <emanuele.serrao@bvtech.com>
2025-01-10 13:27:26 +0100

    fixed

--- src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/EventDispatcherApplication.kt
+++ src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/EventDispatcherApplication.kt
@@ -6,23 +6,20 @@ import org.springframework.context.ApplicationContext
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Lazy
 import org.springframework.integration.config.EnableIntegration
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
 @EnableIntegration
-class EventDispatcherApplication {
+class EventDispatcherApplication(private val applicationContext: ApplicationContext) {
 
-  @Bean
-  fun initializeLazyBeans(applicationContext: ApplicationContext): () -> Unit {
-    return {
-      applicationContext.beanDefinitionNames.forEach { beanName ->
-        applicationContext.getBean(beanName)
-      }
+  @PostConstruct
+  fun initializeLazyBeans() {
+    applicationContext.beanDefinitionNames.forEach { beanName ->
+      applicationContext.getBean(beanName)
     }
   }
 }
 
 fun main(args: Array<String>) {
-  val context = runApplication<EventDispatcherApplication>(*args)
-  val initializeLazyBeans = context.getBean("initializeLazyBeans") as () -> Unit
-  initializeLazyBeans()
+  runApplication<EventDispatcherApplication>(*args)
 }
