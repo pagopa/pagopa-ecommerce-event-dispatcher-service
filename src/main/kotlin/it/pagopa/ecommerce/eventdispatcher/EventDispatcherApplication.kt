f11f540689cd8dfb6c414fb5d4230adf7685a5f3
e.serrao <emanuele.serrao@bvtech.com>
2025-01-10 11:45:16 +0100

    added logic to warmup lazy beans

--- src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/EventDispatcherApplication.kt
+++ src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/EventDispatcherApplication.kt
@@ -1,11 +1,29 @@
 package it.pagopa.ecommerce.eventdispatcher
 
+
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.ApplicationContext
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Lazy
 import org.springframework.integration.config.EnableIntegration
 
-@SpringBootApplication @EnableIntegration class EventDispatcherApplication
+@SpringBootApplication
+@EnableIntegration
+class EventDispatcherApplication {
+
+  @Bean
+  fun initializeLazyBeans(applicationContext: ApplicationContext): () -> Unit {
+    return {
+      applicationContext.getBeansWithAnnotation(Lazy::class.java).forEach { (name, _) ->
+        applicationContext.getBean(name)
+      }
+    }
+  }
+}
 
 fun main(args: Array<String>) {
-  runApplication<EventDispatcherApplication>(*args)
+  val context = runApplication<EventDispatcherApplication>(*args)
+  val initializeLazyBeans = context.getBean("initializeLazyBeans") as () -> Unit
+  initializeLazyBeans()
 }
