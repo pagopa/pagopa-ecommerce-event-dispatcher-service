d098d4982ca3aa58fc2c581e1c3710d6bcd6d8d2
EmanueleBVtech <emanuele.serrao@bvtech.com>
2025-01-13 15:56:55 +0100

    Update StreamConfig.kt

--- src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/config/redis/stream/StreamConfig.kt
+++ src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/config/redis/stream/StreamConfig.kt
@@ -51,7 +51,6 @@ class StreamConfig {
   @Bean
   @InboundChannelAdapter(
     channel = "eventDispatcherReceiverCommandChannel",
-    autoStartup = "false",
     poller = [Poller(fixedDelay = "1000", maxMessagesPerPoll = "1")])
   fun eventDispatcherReceiverCommandMessageSource(
     redisStreamReceiver: StreamReceiver<String, ObjectRecord<String, LinkedHashMap<*, *>>>,
