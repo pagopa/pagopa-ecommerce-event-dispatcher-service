5b192c04e240139853a095a4c11c0a0255a4a9d1
Pietro Tota <pietro.tota@pagopa.it>
2025-06-18 17:47:39 +0200

    chore remove unused lib + update commons ref

--- src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/EventDispatcherApplication.kt
+++ src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/EventDispatcherApplication.kt
@@ -1,11 +1,14 @@
 package it.pagopa.ecommerce.eventdispatcher
 
+import it.pagopa.ecommerce.commons.ConfigScan
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Import
 import org.springframework.integration.config.EnableIntegration
 
-@SpringBootApplication(scanBasePackages = ["it.pagopa.ecommerce"])
+@SpringBootApplication
 @EnableIntegration
+@Import(ConfigScan::class)
 class EventDispatcherApplication
 
 fun main(args: Array<String>) {
