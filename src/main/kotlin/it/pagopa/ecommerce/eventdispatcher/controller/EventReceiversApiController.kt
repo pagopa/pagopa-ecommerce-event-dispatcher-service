9d3dd630a91942d1bfb5d4f6e110a7e57ea6a05e
sfarralorenzo <lorenzo.sfarra@pagopa.it>
2025-07-07 10:49:56 +0200

    chore: code review

--- src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/controller/EventReceiversApiController.kt
+++ src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/controller/EventReceiversApiController.kt
@@ -9,7 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
 
-/** Event receivers commands api controller implementation */
+/** Event receivers commands api controller implementation **/
 @RestController
 class EventReceiversApiController(
   @Autowired private val eventReceiverService: EventReceiverService,
