b63211bf24e056facb4aea6348ac4d59dabbaf9c
Pietro Tota <pietro.tota@pagopa.it>
2025-07-07 10:06:04 +0200

    fix: openapi code generation

--- src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/controller/EventReceiversApiController.kt
+++ src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/controller/EventReceiversApiController.kt
@@ -1,21 +1,19 @@
 package it.pagopa.ecommerce.eventdispatcher.controller
 
 import it.pagopa.ecommerce.eventdispatcher.services.EventReceiverService
-import it.pagopa.generated.eventdispatcher.server.api.EventReceiversApi
+import it.pagopa.generated.eventdispatcher.server.api.EventDispatcherApi
 import it.pagopa.generated.eventdispatcher.server.model.DeploymentVersionDto
 import it.pagopa.generated.eventdispatcher.server.model.EventReceiverCommandRequestDto
 import it.pagopa.generated.eventdispatcher.server.model.EventReceiverStatusResponseDto
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 /** Event receivers commands api controller implementation */
 @RestController
-@RequestMapping("/event-dispatcher")
 class EventReceiversApiController(
   @Autowired private val eventReceiverService: EventReceiverService,
-) : EventReceiversApi {
+) : EventDispatcherApi {
 
   /** Handle new receiver command */
   override suspend fun newReceiverCommand(
