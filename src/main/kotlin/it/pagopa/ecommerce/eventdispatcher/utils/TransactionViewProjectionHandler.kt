e98f0fc10dfb2ee17597b2bbd0bf3f36e0a4e19c
Forciniti Luigi <forciniti@spi.srl>
2025-07-29 16:09:15 +0200

    chore: delete AppPropertiesInitializer.kt  and add init env in TransactionViewProjectionHandler.kt

--- src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/utils/TransactionViewProjectionHandler.kt
+++ src/main/kotlin/it/pagopa/ecommerce/eventdispatcher/utils/TransactionViewProjectionHandler.kt
@@ -2,6 +2,7 @@ package it.pagopa.ecommerce.eventdispatcher.utils
 
 import it.pagopa.ecommerce.commons.documents.v2.Transaction
 import it.pagopa.ecommerce.eventdispatcher.repositories.TransactionsViewRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.core.env.Environment
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
@@ -10,6 +11,12 @@ import reactor.core.publisher.Mono
 object TransactionViewProjectionHandler {
 
     lateinit var env: Environment
+
+    @Autowired
+    fun init(environment: Environment) {
+        env = environment
+    }
+
 
     fun saveEventIntoView(
         baseTransactionView: Transaction,
