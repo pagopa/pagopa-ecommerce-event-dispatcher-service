5f93082edc31303069d68e609078af980c29e2b4
ciuffagianluca <gianluca.ciuffa@pagopa.it>
2026-05-25 22:30:15 +0200

    add tests

--- src/test/kotlin/it/pagopa/ecommerce/eventdispatcher/queues/v2/helpers/AuthorizationRequestedHelperTests.kt
+++ src/test/kotlin/it/pagopa/ecommerce/eventdispatcher/queues/v2/helpers/AuthorizationRequestedHelperTests.kt
@@ -3374,4 +3374,170 @@ class AuthorizationRequestedHelperTests {
       verify(authRequestedQueueAsyncClient, times(0))
         .sendMessageWithResponse(any<BinaryData>(), any(), any())
     }
+
+  @Test
+  fun `messageReceiver return error performing enqueue for auth requested NPG transaction`() =
+    runTest {
+      val transactionActivatedEvent =
+        transactionActivateEvent(npgTransactionGatewayActivationData())
+      val transactionAuthorizationRequestedEvent =
+        transactionAuthorizationRequestedEvent(
+          TransactionAuthorizationRequestData.PaymentGateway.NPG,
+          npgTransactionGatewayAuthorizationRequestedData())
+
+      val paymentInstrumentId = UUID.randomUUID().toString()
+      val authDate =
+        OffsetDateTime.now()
+          .minus(Duration.ofSeconds(firstAttemptOffsetSeconds.toLong()))
+          .toString()
+      transactionAuthorizationRequestedEvent.data.paymentInstrumentId = paymentInstrumentId
+      transactionAuthorizationRequestedEvent.creationDate = authDate
+      val transactionId = TransactionId(TRANSACTION_ID)
+      val events: List<TransactionEvent<Any>> =
+        listOf(
+          transactionActivatedEvent as TransactionEvent<Any>,
+          transactionAuthorizationRequestedEvent as TransactionEvent<Any>)
+
+      val operationId = "operationId"
+      val orderId = "orderId"
+      val authorizationCode = "123456"
+      val rrn = "rrn"
+      val paymentEndToEndId = "paymentEndToEndId"
+      val npgStateResponse =
+        StateResponseDto()
+          .state(WorkflowStateDto.PAYMENT_COMPLETE)
+          .operation(
+            OperationDto()
+              .operationId(operationId)
+              .orderId(orderId)
+              .operationResult(OperationResultDto.EXECUTED)
+              .paymentEndToEndId(paymentEndToEndId)
+              .operationTime("2024-01-01T00:00:00")
+              .additionalData(mapOf("authorizationCode" to authorizationCode, "rrn" to rrn)))
+
+      given(
+          transactionsEventStoreRepository.findByTransactionIdOrderByCreationDateAsc(
+            transactionId.value()))
+        .willReturn(Flux.fromIterable(events))
+      given(authorizationStateRetrieverService.getStateNpg(any(), any(), any(), any(), any()))
+        .willReturn(mono { npgStateResponse })
+      given(transactionsServiceClient.patchAuthRequest(any(), any()))
+        .willReturn(Mono.error(GatewayTimeoutException()))
+      given(authRequestedQueueAsyncClient.sendMessageWithResponse(any<BinaryData>(), any(), any()))
+        .willReturn(
+          Mono.error {
+            TooLateRetryAttemptException(
+              TransactionId(UUID.randomUUID()), "eventCode", Instant.now())
+          })
+      given(
+          authorizationStateRetrieverRetryService.enqueueRetryEvent(
+            any(), any(), any(), anyOrNull(), anyOrNull()))
+        .willReturn(Mono.empty())
+      given(checkpointer.success()).willReturn(Mono.empty())
+
+      // Test
+      StepVerifier.create(
+          authorizationRequestedHelper.authorizationRequestedHandler(
+            QueueEvent(transactionAuthorizationRequestedEvent, TracingInfoTest.MOCK_TRACING_INFO),
+            checkpointer))
+        .expectNext(Unit)
+        .verifyComplete()
+      // assertions
+      verify(authorizationStateRetrieverService, times(1))
+        .getStateNpg(any(), any(), any(), any(), any())
+
+      verify(userStatsServiceClient, times(0)).saveLastUsage(any(), any())
+      verify(deadLetterTracedQueueAsyncClient, times(0))
+        .sendAndTraceDeadLetterQueueEvent(any(), any())
+      verify(authRequestedQueueAsyncClient, times(0))
+        .sendMessageWithResponse(any<BinaryData>(), any(), any())
+      verify(authorizationStateRetrieverRetryService, times(1))
+        .enqueueRetryEvent(any(), any(), any(), anyOrNull(), anyOrNull())
+    }
+
+  @Test
+  fun `messageReceiver return error performing enqueue for auth requested REDIRECT transaction`() =
+    runTest {
+      val transactionActivatedEvent =
+        transactionActivateEvent(npgTransactionGatewayActivationData())
+      val transactionAuthorizationRequestedEvent =
+        transactionAuthorizationRequestedEvent(
+          TransactionAuthorizationRequestData.PaymentGateway.REDIRECT,
+          redirectTransactionGatewayAuthorizationRequestedData())
+
+      val paymentInstrumentId = UUID.randomUUID().toString()
+      val authDate =
+        OffsetDateTime.now()
+          .minus(Duration.ofSeconds(firstAttemptOffsetSeconds.toLong()))
+          .toString()
+      transactionAuthorizationRequestedEvent.data.paymentInstrumentId = paymentInstrumentId
+      transactionAuthorizationRequestedEvent.creationDate = authDate
+
+      val transactionAuthorizationCompletedEvent =
+        transactionAuthorizationCompletedEvent(
+          redirectTransactionGatewayAuthorizationData(
+            RedirectTransactionGatewayAuthorizationData.Outcome.OK, null))
+      val transactionId = TransactionId(TRANSACTION_ID)
+      val events: List<TransactionEvent<Any>> =
+        listOf(
+          transactionActivatedEvent as TransactionEvent<Any>,
+          transactionAuthorizationRequestedEvent as TransactionEvent<Any>,
+          transactionAuthorizationCompletedEvent as TransactionEvent<Any>)
+
+      val operationId = "operationId"
+      val orderId = "orderId"
+      val authorizationCode = "123456"
+      val rrn = "rrn"
+      val paymentEndToEndId = "paymentEndToEndId"
+      val npgStateResponse =
+        StateResponseDto()
+          .state(WorkflowStateDto.PAYMENT_COMPLETE)
+          .operation(
+            OperationDto()
+              .operationId(operationId)
+              .orderId(orderId)
+              .operationResult(OperationResultDto.EXECUTED)
+              .paymentEndToEndId(paymentEndToEndId)
+              .operationTime("2024-01-01T00:00:00")
+              .additionalData(mapOf("authorizationCode" to authorizationCode, "rrn" to rrn)))
+
+      given(
+          transactionsEventStoreRepository.findByTransactionIdOrderByCreationDateAsc(
+            transactionId.value()))
+        .willReturn(Flux.fromIterable(events))
+      given(authorizationStateRetrieverService.getStateNpg(any(), any(), any(), any(), any()))
+        .willReturn(mono { npgStateResponse })
+      given(transactionsServiceClient.patchAuthRequest(any(), any()))
+        .willReturn(Mono.error(GatewayTimeoutException()))
+      given(authRequestedQueueAsyncClient.sendMessageWithResponse(any<BinaryData>(), any(), any()))
+        .willReturn(
+          Mono.error {
+            TooLateRetryAttemptException(
+              TransactionId(UUID.randomUUID()), "eventCode", Instant.now())
+          })
+      given(
+          authorizationStateRetrieverRetryService.enqueueRetryEvent(
+            any(), any(), any(), anyOrNull(), anyOrNull()))
+        .willReturn(Mono.empty())
+      given(checkpointer.success()).willReturn(Mono.empty())
+
+      // Test
+      StepVerifier.create(
+          authorizationRequestedHelper.authorizationRequestedHandler(
+            QueueEvent(transactionAuthorizationRequestedEvent, TracingInfoTest.MOCK_TRACING_INFO),
+            checkpointer))
+        .expectNext(Unit)
+        .verifyComplete()
+      // assertions
+      verify(authorizationStateRetrieverService, times(0))
+        .getStateNpg(any(), any(), any(), any(), any())
+
+      verify(userStatsServiceClient, times(0)).saveLastUsage(any(), any())
+      verify(deadLetterTracedQueueAsyncClient, times(0))
+        .sendAndTraceDeadLetterQueueEvent(any(), any())
+      verify(authRequestedQueueAsyncClient, times(0))
+        .sendMessageWithResponse(any<BinaryData>(), any(), any())
+      verify(authorizationStateRetrieverRetryService, times(1))
+        .enqueueRetryEvent(any(), any(), any(), anyOrNull(), anyOrNull())
+    }
 }
